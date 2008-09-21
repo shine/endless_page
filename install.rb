f232bd872bb78be9b85572b6b81fbc72dd3ea996
Victor Brylew <victor@virlin.MOUSEHOME>
2008-09-21 15:54:38 +0200

    installer fix

--- install.rb
+++ install.rb
@@ -2,6 +2,7 @@ require 'fileutils'
 
 rjs_home = '../../../app/views/endless_page'
 FileUtils.mkdir rjs_home
-FileUtils.cp %w(rjs/append.rjs rjs/have_nothing_to_add.rjs), rjs_home
+FileUtils.cp %w(rjs/append.js.rjs rjs/have_nothing_to_add.js.rjs), 
+rjs_home
 
 FileUtils.cp 'javascripts/endless_page.js', '../../../public/javascripts'
